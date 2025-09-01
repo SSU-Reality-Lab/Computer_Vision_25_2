53471b403b5a17910e91517b7348538c2c5f4408
JEONG-HO-JAE <98eddiechung@gmail.com>
2025-09-01 19:13:00 +0900

    make TO DO

--- Project1_Hybrid_Images/hybrid.py
+++ Project1_Hybrid_Images/hybrid.py
@@ -17,39 +17,6 @@ def cross_correlation_2d(img, kernel):
         kernel: A 2D numpy array (m x n), with m and n both odd (but may not be
                 equal).'''
     
-    u,v = kernel.shape
-    new_img = np.zeros(img.shape)
-
-     #define padding size
-    u_pad = (u - 1) // 2
-    v_pad = (v - 1) // 2
-
-    # if picture is rgb
-    if len(img.shape) > 2:
-        x, y, colors = img.shape
-
-        # create padded image
-        padded_img = np.pad(img, pad_width=((u_pad, u_pad,), (v_pad, v_pad), (0,0)), mode = 'constant', constant_values = 0)
-
-        #loop
-        for i in range(x):
-            for j in range(y):
-                for color in range(colors):
-                    new_img[i, j, color] = np.sum(kernel * padded_img[i:i + u, j:j + v, color])
-
-    else:
-        x,y = img.shape
-
-        # created padded img
-        padded_img = np.pad(img, pad_width=((u_pad, u_pad,), (v_pad, v_pad)), mode = 'constant', constant_values = 0)
-
-        #loop
-        for i in range(x):
-            for j in range(y):
-                new_img[i, j] = np.sum(kernel * padded_img[i:i + u, j:j + v])
-
-    return new_img
-
     '''Output:
         Return an image of the same dimensions as the input image (same width,
         height and the number of color channels)'''
@@ -68,12 +35,7 @@ def convolve_2d(img, kernel):
         Return an image of the same dimensions as the input image (same width,
         height and the number of color channels)
     '''
-    # flip kernel
-    flipped_kernel = np.flip(kernel)
 
-    conv_image = cross_correlation_2d(img, flipped_kernel)
-
-    return conv_image
 
 
 def gaussian_blur_kernel_2d(sigma, height, width):
@@ -92,17 +54,6 @@ def gaussian_blur_kernel_2d(sigma, height, width):
         with an image results in a Gaussian-blurred image.
     '''
 
-    gauss_kernel = np.zeros((height, width))
-    h = np.linspace(-height / 2 + 1, height / 2, height)
-    w = np.linspace(-width / 2 + 1, width / 2, width)
-
-    for x, x1 in enumerate(h):
-        for y, y1 in enumerate(w):
-            gauss_kernel[x,y] = 1 / (2 * np.pi * (sigma ** 2)) * np.exp(-(x1 ** 2 + y1 ** 2)/(2 * (sigma ** 2)))
-    
-    norm_gauss = gauss_kernel * 1 / np.sum(gauss_kernel)
-    
-    return norm_gauss
 
 
 def low_pass(img, sigma, size):
@@ -115,7 +66,7 @@ def low_pass(img, sigma, size):
         height and the number of color channels)
     '''
 
-    return convolve_2d(img, gaussian_blur_kernel_2d(sigma, size, size))
+ 
 
 
 def high_pass(img, sigma, size):
@@ -128,32 +79,11 @@ def high_pass(img, sigma, size):
         height and the number of color channels)
     '''
 
-    return img - low_pass(img, sigma, size)
 
 
 def create_hybrid_image(img1, img2, sigma1, size1, high_low1, sigma2, size2,
         high_low2, mixin_ratio, scale_factor):
     '''This function adds two images to create a hybrid image, based on
     parameters specified by the user.'''
-    high_low1 = high_low1.lower()
-    high_low2 = high_low2.lower()
 
-    if img1.dtype == np.uint8:
-        img1 = img1.astype(np.float32) / 255.0
-        img2 = img2.astype(np.float32) / 255.0
-
-    if high_low1 == 'low':
-        img1 = low_pass(img1, sigma1, size1)
-    else:
-        img1 = high_pass(img1, sigma1, size1)
-
-    if high_low2 == 'low':
-        img2 = low_pass(img2, sigma2, size2)
-    else:
-        img2 = high_pass(img2, sigma2, size2)
-
-    img1 *=  (1 - mixin_ratio)
-    img2 *= mixin_ratio
-    hybrid_img = (img1 + img2) * scale_factor
-    return (hybrid_img * 255).clip(0, 255).astype(np.uint8)
 
